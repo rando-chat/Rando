e58609913302bff9853dbd333f95b44e13b4c6a6
Newton-ait <newashiraf@gmail.com>
2026-02-10 01:32:00 +0300

    Describe your latest changes

--- app/chat/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/chat/page.tsx
-import ChatPage from '../src/pages/ChatPage';
-
-export default function Chat() {
-  return <ChatPage />;
-}
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/page.tsx
-import LandingPage from './src/pages/LandingPage';
-
-export default function Home() {
-  return <LandingPage />;
-}
