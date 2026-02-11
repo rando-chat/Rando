407966699c910912476709d2fc763eaf8619c3cd
d3skdevelopers <d3skdevelopers@gmail.com>
2026-02-11 11:09:10 +0300

    Update page.tsx

--- app/matchmaking/page.tsx
+++ app/matchmaking/page.tsx
@@ -1,16 +1,6 @@
-'use client'
-
 import { MatchmakingQueue } from '@/components/matchmaking/MatchmakingQueue'
-import { AuthGuard } from '@/components/auth/AuthGuard'
 
+// No AuthGuard - guests can chat without signing in
 export default function MatchmakingPage() {
-  return (
-    <AuthGuard>
-      <div className="min-h-screen bg-gradient-to-br from-purple-50 to-blue-50">
-        <div className="container mx-auto px-4 py-8">
-          <MatchmakingQueue />
-        </div>
-      </div>
-    </AuthGuard>
-  )
+  return <MatchmakingQueue />
 }
